f75a1f994f44ce45f1dd3db7ce8fabe184c51756
Nicholas Tennies <nick@brainbytescreative.com>
2015-12-16 17:52:42 -0500

    removes layer slider

--- wp-content/themes/avian/index.php
+++ wp-content/themes/avian/index.php
@@ -1,7 +1,5 @@
 <?php get_header();?>
 
-<div class="layer-slider"></div>
-
 <?php if (!is_tag()) { ?>
 <!-- Breadcrumb -->
 <div class="page-callout">
@@ -10,7 +8,7 @@
 			<div class="span6">
 				<?php zt_the_breadcrumb_data(); ?>
 			</div>
-			
+
 			<div class="span6">
 				<div class="page-name">
 					<?php the_title(); ?>
@@ -28,10 +26,10 @@
 		<div class="row">
 			<div class="span6">
 				<div class="breadcrumbs">
-					Tag: 
+					Tag:
 					<ul>
 						<li>
-							<?php single_tag_title(); ?> 
+							<?php single_tag_title(); ?>
 						</li>
 					</ul>
 				</div>
@@ -44,20 +42,20 @@
 <div class="content-container">
 <div class="container">
 	<div class="row">
-		
+
 		<div class="span8">
-			
+
 			<?php while ( have_posts() ) : the_post(); ?>
-			
+
 			<div class="row">
-				
+
 				<div class="span8">
-							
+
 					<?php
 					global $more;
 					$more = 0;
 					?>
-					
+
 					<div class="classic">
 						<?php if(!get_post_format()) {
 							get_template_part('post/blog/'.'standard');
@@ -66,18 +64,18 @@
 						}
 						?>
 					</div>
-				
+
 				</div>
-				
+
 			</div>
 			<?php endwhile; // end of the loop. ?>
 			<div class="pagination"><p><?php posts_nav_link('&nbsp;','Previous page','Next page'); ?></p></div>
 		</div>
-						
+
 		<div class="span4 widget-right">
 			<?php dynamic_sidebar('Page'); ?>
-		</div>	
-		
+		</div>
+
 	</div>
 </div>
 
@@ -86,4 +84,4 @@
 </div>
 
 <?php wp_reset_query(); ?>
-<?php get_footer(); ?>   
+<?php get_footer(); ?>
--- wp-content/themes/avian/page.php
+++ wp-content/themes/avian/page.php
@@ -21,12 +21,12 @@ if ($sidebar == "") {
 ?>
 
 <!-- Slider -->
-<div class="layer-slider">
-	<?php if (get_field('layerslider_id')) { ?>
-		<?php $layerslider_id = get_field('layerslider_id'); ?>
-		<?php echo do_shortcode("[layerslider id='$layerslider_id']"); ?>
-	<?php } ?>
-</div>
+<!-- <div class="layer-slider">
+	<?php //if (get_field('layerslider_id')) { ?>
+		<?php //$layerslider_id = get_field('layerslider_id'); ?>
+		<?php //echo do_shortcode("[layerslider id='$layerslider_id']"); ?>
+	<?php //} ?>
+</div> -->
 
 <!-- Breadcrumb -->
 <?php if (get_field('breadcrumb') == true) { zt_the_breadcrumb(); } ?>
@@ -34,9 +34,9 @@ if ($sidebar == "") {
 <!-- Begin content -->
 <div class="content-container">
 	<div class="container">
-	
+
 		<div class="row">
-		
+
 			<!-- Left sidebar -->
 			<?php if ($sidebar == "left") { ?>
 				<div class="span4 widget-area widget-left">
@@ -47,21 +47,21 @@ if ($sidebar == "") {
 					<?php } ?>
 				</div>
 			<?php } ?>
-			
+
 			<!-- MAIN CONTENT -->
 			<div class="<?php echo $span_size; ?>">
-				
+
 			<?php while (have_posts()) : the_post(); ?>
 				<?php $my_terms = get_the_terms( $post->ID, 'Skills' ); ?>
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 					<?php the_content(); ?>
-											
+
 				</div>
 			<?php endwhile; ?>
-			
+
 			</div>
-			
+
 			<!-- Right sidebar -->
 			<?php if ($sidebar == "right") { ?>
 				<div class="span4 widget-area widget-right">
@@ -72,9 +72,9 @@ if ($sidebar == "") {
 					<?php } ?>
 				</div>
 			<?php } ?>
-			
+
 		</div>
-		
+
 	</div>
 </div>
-<?php get_footer(); ?>   
+<?php get_footer(); ?>
