42ac86176b7175e89c12f4c58f7bfba4e2e902cc
Nicholas Tennies <nick@brainbytescreative.com>
2015-12-30 12:43:42 -0500

    adds What you can expect js

--- wp-content/themes/hodgesmace/js/functions.js
+++ wp-content/themes/hodgesmace/js/functions.js
@@ -9,4 +9,13 @@
     $('.header_nav').toggle();
   })
 
+  // Internal What You Can Expect
+
+  $('.hover').hover(function(e){
+    debugger;
+    var idx = $('.hover').index(this);
+    $('.hover-contet').hide();
+    $('.hover-content-'+idx).show();
+  });
+
 })(jQuery);
