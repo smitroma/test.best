7556978f806e9ec89d62b53cbeb6a6dbd73b93a4
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-04 17:07:46 -0500

    add icon margin

--- wp-content/themes/hodgesmace/searchform.php
+++ wp-content/themes/hodgesmace/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div>
 		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" style="margin-right: 10px;"/>
 		<?php echo do_shortcode('[icon class="fa-search"]') ?>
 	</div>
 </form>
