9a27ce04a4d4e1cf3bd9283db614bd776110c329
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-04 14:59:57 -0500

    removes topnav

--- wp-content/themes/hodgesmace/header.php
+++ wp-content/themes/hodgesmace/header.php
@@ -26,7 +26,7 @@
 		<div id="top-header">
 			<div class="container">
 				<?php echo do_shortcode('[icon class="fa-search"]') ?>
-				<?php wp_nav_menu(array('menu' => 'Login Nav', 'menu_class' => 'top_nav' )); ?>
+				
 				<!-- <a href="#">
 					<button class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-rounded vc_btn3-style-modern bttn-white-border m-l-md">SmartBen Login</button>
 				</a> -->
