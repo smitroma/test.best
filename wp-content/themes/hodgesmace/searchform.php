91a4debee78ac0c480cfafc648791ecefbcf4bf5
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-04 16:19:33 -0500

    adds icon to button

--- wp-content/themes/hodgesmace/searchform.php
+++ wp-content/themes/hodgesmace/searchform.php
@@ -2,6 +2,6 @@
 	<div>
 		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
 		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-		<input type="submit" id="searchsubmit" value="" />
+		<input type="submit" id="searchsubmit" value="&#xf002;" />
 	</div>
 </form>
