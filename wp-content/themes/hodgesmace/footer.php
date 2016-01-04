82f0e8c91a2d98c63005f0d4d9e11727169d2056
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-04 14:39:28 -0500

    adjust footer spacing

--- wp-content/themes/hodgesmace/footer.php
+++ wp-content/themes/hodgesmace/footer.php
@@ -12,9 +12,9 @@
 </div><!-- .site-content -->
 <div id="footer">
   <div class="row container">
-    <p class="left c-white">&copy; <?php echo date('Y') ?> <?php bloginfo('name')?></p>  
+    <p class="left c-white">&copy; <?php echo date('Y') ?> <?php bloginfo('name')?></p>
     <p class="right c-white">
-      <?php echo do_shortcode('[icon class="fa-linkedin"]') ?> <?php echo do_shortcode('[icon class="fa-envelope"]')?>
+      <?php echo do_shortcode('[icon class="fa-linkedin m-r-10-px"]') ?> <?php echo do_shortcode('[icon class="fa-envelope"]')?>
     </p>
     <?php wp_nav_menu(array('menu' => 'Footer Menu', 'menu_class' => 'footer_nav' )); ?>
   </div>
