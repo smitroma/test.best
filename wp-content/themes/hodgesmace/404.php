954ee45c4a858fe64c4fb0a46b68f181696f3a0b
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-06 12:37:07 -0500

    completes 404 page

--- wp-content/themes/hodgesmace/404.php
+++ wp-content/themes/hodgesmace/404.php
@@ -3,7 +3,7 @@
  * The template for displaying 404 pages (not found)
  *
  * @package Brain_Bytes
- * @subpackage Hodges Mace 
+ * @subpackage Hodges Mace
  * @since Hodges Mace  1.0
  */
 
@@ -11,13 +11,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<section class="error-404 not-found">
-				<header class="page-header">
+			<section class="error-404 not-found container p-y-lg">
+				<header class="page-header ta-c">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'hodgesmace' ); ?></h1>
 				</header>
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'hodgesmace' ); ?></p>
-					<?php get_search_form(); ?>
+					<p class="ta-c"><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'hodgesmace' ); ?></p>
+					<div class="aligncenter d-t p-y-md"><?php get_search_form(); ?></div>
 				</div>
 			</section>
 		</main>
