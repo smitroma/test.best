b2784c2b8f177f37a034a42bfb31f70e9e50045a
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-14 12:50:51 -0500

    set even column height responsive

--- wp-content/themes/hodgesmace/js/functions.js
+++ wp-content/themes/hodgesmace/js/functions.js
@@ -5,47 +5,70 @@
 (function($) {
 
   // Mobile Menu toggle
-  $('.mobile-menu-toggle').click(function() {
-    $('.header_nav').toggle();
-  })
+    $('.mobile-menu-toggle').click(function() {
+      $('.header_nav').toggle();
+    })
 
   // Internal What You Can Expect
 
-  var getWhatYouExpectContent = function(activeBox) {
-    var hoverContent = '.hover-content-'+$('.hover').index(activeBox);
-    $('.hover').removeClass('hover-active');
-    $(activeBox).addClass('hover-active');
-    $('.hover-content').hide();
-    return(hoverContent);
-  }
-
-  // Desktop
-  $('.hover').hover(function(e){
-    var hoverContent = getWhatYouExpectContent(this);
-    console.log(hoverContent);
-    $(hoverContent).show();
+    var getWhatYouExpectContent = function(activeBox) {
+      var hoverContent = '.hover-content-'+$('.hover').index(activeBox);
+      $('.hover').removeClass('hover-active');
+      $(activeBox).addClass('hover-active');
+      $('.hover-content').hide();
+      return(hoverContent);
     }
-  );
 
-  // Mobile
-  $('.hover').click(function(e){
-    var hoverContent = getWhatYouExpectContent(this);
-    $(hoverContent).show();
-    }
-  );
+    // Desktop - Internal What You Can Expect
+      $('.hover').hover(function(e){
+        var hoverContent = getWhatYouExpectContent(this);
+        console.log(hoverContent);
+        $(hoverContent).show();
+        }
+      );
 
-  // Anchor
-  $('.anchor-bttn').click(function() {
-    var anchorTarget = $(this).attr('data-target');
-    $('html, body').animate({
-        scrollTop: $('#'+anchorTarget).offset().top
-    }, 1000);
-  })
+    // Mobile - Internal What You Can Expect
+      $('.hover').click(function(e){
+        var hoverContent = getWhatYouExpectContent(this);
+        $(hoverContent).show();
+        }
+      );
+
+  // Anchor Buttons
+    $('.anchor-bttn').click(function() {
+      var anchorTarget = $(this).attr('data-target');
+      $('html, body').animate({
+          scrollTop: $('#'+anchorTarget).offset().top
+      }, 1000);
+    })
 
   // SmartBen Boxes
-  $('.smartben-box').click(function() {
-    $('.smartben-hover-content').hide();
-    $(this).find('.smartben-hover-content').show();
-  })
+    $('.smartben-box').click(function() {
+      $('.smartben-hover-content').hide();
+      $(this).find('.smartben-hover-content').show();
+    });
+
+  // Even Column Height
+    var setEvenColHeight = function() {
+      $('.even-col-row').each(function() {
+        var colHeight = 0;
+        $('.even-col-row .even-col').each(function() {
+          $(this).css('height', 'auto');
+          thisHeight = $(this).height();
+          colHeight = (thisHeight > colHeight) ? thisHeight : colHeight;
+        });
+        $('.even-col').each(function(){
+          $(this).height(colHeight);
+        });
+      });
+    }
+
+    // onLoad - Even Column Height
+      setEvenColHeight();
+
+    // onResize - Even Column Height
+      $(window).resize(function() {
+        setEvenColHeight();
+      });
 
 })(jQuery);
