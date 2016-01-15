936eeccfc103ab7e765946ca7099b80da6ea2f8a
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-15 11:51:21 -0500

    added acton tracking code to footer

--- wp-content/themes/hodgesmace/footer.php
+++ wp-content/themes/hodgesmace/footer.php
@@ -21,5 +21,9 @@
 </div>
 </div><!-- .container-fluid -->
 <?php wp_footer(); ?>
+
+<!-- ActOn Tracking Code -->
+<script>/*<![CDATA[*/(function(w,a,b,d,s){w[a]=w[a]||{};w[a][b]=w[a][b]||{q:[],track:function(r,e,t){this.q.push({r:r,e:e,t:t||+new Date});}};var e=d.createElement(s);var f=d.getElementsByTagName(s)[0];e.async=1;e.src='//marketing.hodgesmace.com/cdnr/96/acton/bn/tracker/17907';f.parentNode.insertBefore(e,f);})(window,'ActOn','Beacon',document,'script');ActOn.Beacon.track();/*]]>*/</script>
+
 </body>
 </html>
