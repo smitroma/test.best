4c012f898068afa74df390eb04fa4afe24c4e074
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-21 15:16:28 -0500

    fixes typo

--- wp-content/themes/hodgesmace/header.php
+++ wp-content/themes/hodgesmace/header.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	<!--[if lt IE 9]>
+	<!--[if lte IE 9]>
 	   <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 		 <link rel="stylesheet" type="text/css" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/ie.css">
 	<![endif]-->
