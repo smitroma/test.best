76757fb6aa394157a15d5d5e61d7eb3d6c0b8974
Nicholas Tennies <nick@brainbytescreative.com>
2016-01-25 13:08:42 -0500

    removes dup meta

--- wp-content/themes/hodgesmace/header.php
+++ wp-content/themes/hodgesmace/header.php
@@ -10,7 +10,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js" prefix="og: http://ogp.me/ns#">
 <head>
-	<meta property="og:image" content="<?php echo get_template_directory_uri ()?>/images/hodgesmace-logo-hi.png">	
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
