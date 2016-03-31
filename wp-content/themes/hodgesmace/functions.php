12dd2f24959923b3e85eee91113cf3526513c0ab
Nick Tennies <nicktennies@Nicks-MacBook-Pro.local>
2016-03-31 11:07:08 -0400

    removes html5 gallery support

--- wp-content/themes/hodgesmace/functions.php
+++ wp-content/themes/hodgesmace/functions.php
@@ -44,7 +44,7 @@ if (!function_exists('hodges_setup')) {
          * to output valid HTML5.
          */
         add_theme_support('html5', array(
-            'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+            'search-form', 'comment-form', 'comment-list', 'caption'
         ));
 
         /*
