336704e9a0a3c98b3fe84c297c6c3899f3a1efff
Nick Tennies <nick@brainbytescreative.com>
2016-04-05 12:17:33 -0400

    pushes new state field to acton

--- wp-content/themes/hodgesmace/functions.php
+++ wp-content/themes/hodgesmace/functions.php
@@ -440,7 +440,7 @@ function send_to_acton_5($entry,$form) {
   $ao_gf1->setPostItems('businessPhone',$entry['12']);
   $ao_gf1->setPostItems('productInterest',join(',', array_filter($interestSolutions)));
   $ao_gf1->setPostItems('companyName',$entry['15']);
-  $ao_gf1->setPostItems('state',$entry['16']);
+  $ao_gf1->setPostItems('state',$entry['22']);
   $ao_gf1->setPostItems('Number of Employees Range',$entry['20']);
   $ao_gf1->setPostItems('Industry',$entry['19']);
 
