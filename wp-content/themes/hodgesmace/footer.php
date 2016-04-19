c4ce2786352fb6c7c2c0fa317bc2b706d009dc61
Nick Tennies <nick@brainbytescreative.com>
2016-04-19 15:10:17 -0400

    adds beacon tracking code

--- wp-content/themes/hodgesmace/footer.php
+++ wp-content/themes/hodgesmace/footer.php
@@ -21,8 +21,8 @@
 </div>
 </div><!-- .container-fluid -->
 
-<!-- Hello Bar -->
-<!-- <script src="//my.hellobar.com/7ab6f26df44190c1fc207f819a72fcad0185b7c6.js" type="text/javascript" charset="utf-8" async="async"></script> -->
+<!-- Act on Beacon Tracking -->
+<script>/*<![CDATA[*/(function(w,a,b,d,s){w[a]=w[a]||{};w[a][b]=w[a][b]||{q:[],track:function(r,e,t){this.q.push({r:r,e:e,t:t||+new Date});}};var e=d.createElement(s);var f=d.getElementsByTagName(s)[0];e.async=1;e.src='//marketing.hodgesmace.com/cdnr/96/acton/bn/tracker/17907';f.parentNode.insertBefore(e,f);})(window,'ActOn','Beacon',document,'script');ActOn.Beacon.track();/*]]>*/</script>
 
 <?php wp_footer(); ?>
 </body>
