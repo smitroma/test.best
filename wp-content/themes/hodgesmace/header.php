b8869e8e07d5610f04fa6da84a4b7dc9abd1ca76
Nick Tennies <nick@brainbytescreative.com>
2016-06-02 14:04:40 -0400

    corrects typo

--- wp-content/themes/hodgesmace/header.php
+++ wp-content/themes/hodgesmace/header.php
@@ -59,7 +59,7 @@
 								<p>
 									<a href="/request-a-demo-broker" class="bttn-blue">Broker</a>
 									<a href="/request-a-demo-company" class="bttn-blue">Employer</a>
-									<a href="/request-a-demo-carrier" class="bttn-blue">Employer</a>
+									<a href="/request-a-demo-carrier" class="bttn-blue">Carrier</a>
 								</p>
 							</div>
 						</div>
