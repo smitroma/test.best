1f6a89d0b45772ac086cbfecdbd3dfc9f8f9d49f
jlee <jlee@brainbytescreative.com>
2016-08-22 15:48:06 -0400

    Added FFox script for text placement on buttons

---
+++ wp-content/themes/hodgesmace/images/sb-infographic/tests.js
@@ -0,0 +1,8 @@
+if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
+     // Do Firefox-related activities
+     document.getElementById("personalizationTextPath").setAttribute("startOffset", "-1%");
+     document.getElementById("educationTextPath").setAttribute("startOffset", "86.2%");
+     document.getElementById("eligibilityTextPath").setAttribute("startOffset", "2%");
+     document.getElementById("complianceTextPath").setAttribute("startOffset", "2%");
+     document.getElementById("decisionTextPath").setAttribute("startOffset", "-.8%");
+}
