16af4fa3e0ac0744f93bbbc8b95fd2106e42cb32
David Kirby <kirby@brainbytescreative.com>
2016-08-22 23:23:41 -0400

    Clean up WP script versioning

--- wp-content/themes/hodgesmace/functions.php
+++ wp-content/themes/hodgesmace/functions.php
@@ -635,4 +635,10 @@ function magazino_widgets_init() {
 
 }
 add_action( 'widgets_init', 'magazino_widgets_init' );
-?>
+
+function _remove_script_version($src) {
+  $parts = explode( '?ver', $src );
+  return $parts[0];
+}
+add_filter('script_loader_src', '_remove_script_version', 15, 1);
+add_filter('style_loader_src', '_remove_script_version', 15, 1);
