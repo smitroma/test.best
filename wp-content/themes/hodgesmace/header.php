720f5ca81bb633fb6d31740a00cb4e8bf93fed64
Scott Adams <ScottAdams@ScottAdams.me>
2016-11-11 14:04:55 -0500

    formating of Act-On beacon tracking script

--- wp-content/themes/hodgesmace/header.php
+++ wp-content/themes/hodgesmace/header.php
@@ -21,29 +21,7 @@
 	<![endif]-->
 	<?php wp_head(); ?>
 
-	<script>
-		/*<![CDATA[*/
-		(function(w, a, b, d, s) {
-		    w[a] = w[a] || {};
-		    w[a][b] = w[a][b] || {
-		        q: [],
-		        track: function(r, e, t) {
-		            this.q.push({
-		                r: r,
-		                e: e,
-		                t: t || +new Date
-		            });
-		        }
-		    };
-		    var e = d.createElement(s);
-		    var f = d.getElementsByTagName(s)[0];
-		    e.async = 1;
-		    e.src = '//marketing.hodgesmace.com/cdnr/96/acton/bn/tracker/17907';
-		    f.parentNode.insertBefore(e, f);
-		})(window, 'ActOn', 'Beacon', document, 'script');
-		ActOn.Beacon.track();
-		/*]]>*/
-		</script>
+<script>/*<![CDATA[*/(function(w,a,b,d,s){w[a]=w[a]||{};w[a][b]=w[a][b]||{q:[],track:function(r,e,t){this.q.push({r:r,e:e,t:t||+new Date});}};var e=d.createElement(s);var f=d.getElementsByTagName(s)[0];e.async=1;e.src='//marketing.hodgesmace.com/cdnr/96/acton/bn/tracker/17907';f.parentNode.insertBefore(e,f);})(window,'ActOn','Beacon',document,'script');ActOn.Beacon.track();/*]]>*/</script>
 </head>
 <body <?php body_class(); ?>>
 
