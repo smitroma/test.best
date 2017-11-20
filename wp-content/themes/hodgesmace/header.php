a4532ac691e758f9f9a27960059755bbfd3271e3
J Lee <jlee@brainbytescreative.com>
2017-11-20 15:46:23 -0500

    adds optimize to header

--- wp-content/themes/hodgesmace/header.php
+++ wp-content/themes/hodgesmace/header.php
@@ -8,6 +8,7 @@
  */
 ?>
 <!DOCTYPE html>
+
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -32,6 +33,16 @@
 		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 		})(window,document,'script','dataLayer','GTM-NCHLPG');</script>
 	<!-- End Google Tag Manager -->
+<!-- Page hiding snippet (recommended) -->
+<style>.async-hide { opacity: 0 !important} </style>
+<script>
+(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);
+})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-NCHLPG':true});
+</script>
+<!-- END GOOGLE OPTIMIZE-->
+
 
   <div class="container-fluid">
 		<div id="top-header">
