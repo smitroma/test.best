ae4d76b141984c5dd56d7a3e76609eda6b1181f1
Goddard <joseph@brainbytescreative.com>
2018-05-31 01:04:08 -0400

    btn

--- wp-content/themes/hodgesmace/header.php
+++ wp-content/themes/hodgesmace/header.php
@@ -50,7 +50,7 @@ h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 			<div class="container">
 				<?php get_search_form( true ); ?>
 				<?php wp_nav_menu(array('menu' => 'Login Menu', 'menu_class' => 'top_nav' )); ?>
-				<button id="btn-control" class="bttn-orange">Request a Demo</button>
+				<button id="btn-control" class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-rounded vc_btn3-style-modern bttn-orange m-l-sm requestDemoBtn">Request a Demo</button>
 			</div>
 		</div>
     <div id="header">
