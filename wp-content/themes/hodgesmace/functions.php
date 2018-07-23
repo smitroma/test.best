45af41c6990b1e32ed1394e0bf6c8aee68958a7e
Goddard <joseph@brainbytescreative.com>
2018-07-23 10:15:07 -0400

    defer parsing of js

--- wp-content/themes/hodgesmace/functions.php
+++ wp-content/themes/hodgesmace/functions.php
@@ -794,3 +794,11 @@ return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
 
 endforeach;
 }
+
+// Defer Parsing of js
+function defer_parsing_of_js ( $url ) {
+if ( FALSE === strpos( $url, '.js' ) ) return $url;
+if ( strpos( $url, 'jquery.js' ) ) return $url;
+return "$url' defer ";
+}
+add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
