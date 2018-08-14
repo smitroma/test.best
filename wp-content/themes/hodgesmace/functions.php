bbbe0c12817de0e68d5a0a4913e8d80b4d69646a
Goddard <joseph@brainbytescreative.com>
2018-08-14 14:17:36 -0400

    comment out js parsing

--- wp-content/themes/hodgesmace/functions.php
+++ wp-content/themes/hodgesmace/functions.php
@@ -796,9 +796,9 @@ endforeach;
 }
 
 // Defer Parsing of js
-function defer_parsing_of_js ( $url ) {
-if ( FALSE === strpos( $url, '.js' ) ) return $url;
-if ( strpos( $url, 'jquery.js' ) ) return $url;
-return "$url' defer ";
-}
-add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+// function defer_parsing_of_js ( $url ) {
+// if ( FALSE === strpos( $url, '.js' ) ) return $url;
+// if ( strpos( $url, 'jquery.js' ) ) return $url;
+// return "$url' defer ";
+// }
+// add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
